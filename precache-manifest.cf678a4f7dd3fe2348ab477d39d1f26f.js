57f289218104d53ee80e9549559769eb1a54de5f
unknown <lewiswinthrop27@gmail.com>
2020-06-11 02:24:05 +0100

    Updates

---
+++ precache-manifest.cf678a4f7dd3fe2348ab477d39d1f26f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9c2280f433417bc7474fa1102806fb",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "b9bcad58e9c33747dd8a",
+    "url": "/react-quiz-app/static/css/main.7facf4dc.chunk.css"
+  },
+  {
+    "revision": "65be7723b45143d367b5",
+    "url": "/react-quiz-app/static/js/2.ab4cc0af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-quiz-app/static/js/2.ab4cc0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9bcad58e9c33747dd8a",
+    "url": "/react-quiz-app/static/js/main.dcb990db.chunk.js"
+  },
+  {
+    "revision": "9eb58b5c9c3683c2b8e8",
+    "url": "/react-quiz-app/static/js/runtime-main.3525832f.js"
+  }
+]);
